bc407b09d87182a786b0f8c300f274f904f15802
Ashwini <amurthy.ashwini@gmail.com>
2023-07-20 19:08:45 +0200

    Added context and moved logic inside the context

---
+++ src/contexts/quiz.js
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+import { useReducer } from "react";
+
+const initialState = {
+    currentQuestionIndex: 0,
+    questions: [],
+};
+
+const reducer = (state, action) => {
+    if (action.type === 'NEXT_QUESTION') {
+        return {
+            ...state,
+            currentQuestionIndex: state.currentQuestionIndex + 1,
+
+        };
+    }
+};
+
+export const QuizContext = createContext();
+
+export const QuizProvider = ({ children }) => {
+
+    const value = useReducer(reducer, initialState);
+    console.log("state", value);
+    return (
+        <QuizContext.Provider value={value} >
+            {children}
+        </QuizContext.Provider>);
+
+};
