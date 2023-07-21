d8d7735fdbcb4e5cd891b9e53ef02f19580698d0
Ashwini <amurthy.ashwini@gmail.com>
2023-07-21 12:59:37 +0200

    Added Restart logic in Context

--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -1,18 +1,32 @@
 import { createContext } from "react";
 import { useReducer } from "react";
+import questions from "../data";
 
 const initialState = {
     currentQuestionIndex: 0,
-    questions: [],
+    questions,
+    showResults: false,
 };
 
 const reducer = (state, action) => {
-    if (action.type === 'NEXT_QUESTION') {
-        return {
-            ...state,
-            currentQuestionIndex: state.currentQuestionIndex + 1,
 
-        };
+
+
+    switch (action.type) {
+        case 'NEXT_QUESTION':
+            {
+                const showResults = state.currentQuestionIndex === state.questions.length - 1;
+                const currentQuestionIndex = showResults ? state.currentQuestionIndex : state.currentQuestionIndex + 1;
+                return {
+                    ...state,
+                    currentQuestionIndex,
+                    showResults,
+
+                }
+            }
+        case 'RESTART':
+            return initialState;
+
     }
 };
 
@@ -21,7 +35,6 @@ export const QuizContext = createContext();
 export const QuizProvider = ({ children }) => {
 
     const value = useReducer(reducer, initialState);
-    console.log("state", value);
     return (
         <QuizContext.Provider value={value} >
             {children}
