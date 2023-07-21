545f677782b7e657e3c1c696d05f6d6b4006fbac
Ashwini <amurthy.ashwini@gmail.com>
2023-07-21 12:59:10 +0200

    Added restart functionality and results page

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -10,13 +10,29 @@ const Quiz = () => {
 
     return (
         <div className="quiz">
-            <div className="score" >Question 1/8</div>
-            <Question />
-            <div
-                className="next-button"
-                onClick={() => dispatch({ type: "NEXT_QUESTION" })}
-            >Next Question
-            </div>
+            {quizState.showResults && (
+                <div className="results">
+                    <div className="congratulations">Congratulations!!</div>
+                    <div className="results-info">
+                        <div>You've completed the quiz successfully! </div>
+                        <div>Your score is x of {quizState.questions.length}  </div>
+                    </div>
+                    <div
+                        className="next-button"
+                        onClick={() => dispatch({ type: "RESTART" })}
+                    >Restart
+                    </div>
+                </div>)}
+            {!quizState.showResults && (
+                <div>
+                    <div className="score" >{`Question ${quizState.currentQuestionIndex + 1}/${quizState.questions.length}`}</div>
+                    <Question />
+                    <div
+                        className="next-button"
+                        onClick={() => dispatch({ type: "NEXT_QUESTION" })}
+                    >Next Question
+                    </div>
+                </div>)}
         </div>
     );
 };
