09a73b13e9b5413ca17de0134c9d74eadd48f684
Ashwini <amurthy.ashwini@gmail.com>
2023-11-01 19:40:42 +0100

    added default propTypes

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
+import PropTypes from 'prop-types';
 
 function FeedbackList({ feedback }) {
     {
@@ -17,5 +18,15 @@ function FeedbackList({ feedback }) {
         </div>
     );
 }
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            rating: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+        })
+    )
+};
 
 export default FeedbackList;
