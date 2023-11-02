2a210a22612871605627d722875abcdb49418be3
Ashwini <amurthy.ashwini@gmail.com>
2023-11-02 19:54:24 +0100

    formatting changes

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
-import FeedbackItem from './FeedbackItem';
-import PropTypes from 'prop-types';
+import React from "react";
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
 
 function FeedbackList({ feedback, handleDelete }) {
-    {
-        !feedback || feedback.length &&
-            <p>No Feedback yet</p>;
-    }
+  {
+    !feedback || (feedback.length && <p>No Feedback yet</p>);
+  }
 
-    return (
-        <div className='feedback-list'>
-            {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-
-            ))}
-
-        </div>
-    );
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  );
 }
 
 FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            rating: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-        })
-    )
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default FeedbackList;
