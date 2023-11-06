20c5c8c562ac1454e99ca97135c06dd472085062
Ashwini <amurthy.ashwini@gmail.com>
2023-11-06 12:06:36 +0100

    added rating buttons

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Card from "./shared/Card";
 import { useState } from "react";
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+  const [rating, setRating] = useState(10);
 
   const handleTextChange = (e) => {
     if (text === "") {
@@ -26,7 +28,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* Rating select component */}
+        <RatingSelect select={(selectedRating) => setRating(selectedRating)} />
         <div className="input-group">
           <input
             type="text"
