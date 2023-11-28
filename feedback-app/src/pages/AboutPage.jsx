e94da95bcf3b0ccb5273af94e11b5c8df0d7bc14
Ashwini <amurthy.ashwini@gmail.com>
2023-11-28 15:07:33 +0100

    bug fix

--- feedback-app/src/pages/AboutPage.jsx
+++ feedback-app/src/pages/AboutPage.jsx
@@ -5,14 +5,15 @@ import { Link } from "react-router-dom";
 function AboutPage() {
   return (
     <Card>
-      <div className="about"></div>
-      <h1>About this project</h1>
-      <p>This is a React app to leave feedback for a product or service</p>
-      <p>version: 1.0.0</p>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>This is a React app to leave feedback for a product or service</p>
+        <p>version: 1.0.0</p>
 
-      <p>
-        <Link to="/">Back to Home</Link>
-      </p>
+        <p>
+          <Link to="/">Back to Home</Link>
+        </p>
+      </div>
     </Card>
   );
 }
