782cc6c22d7ba6de0e6a009df71a07a41bf1d904
Ashwini <amurthy.ashwini@gmail.com>
2023-12-04 16:11:10 +0100

    added mock backend

--- feedback-app/README.md
+++ feedback-app/README.md
@@ -1,1 +1,4 @@
 React app that can be used to take Feedbacks on products and services.
+
+Deployed using Netlify:
+https://splendid-profiterole-c87f93.netlify.app
--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -1,31 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is from Context 1",
-      rating: 8,
-    },
-    {
-      id: 2,
-      text: "This is from Context 2 ",
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: "This is from Context 3",
-      rating: 10,
-    },
-  ]);
-
+  const [loading, isLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  //Fetch Feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      "http://localhost:5000/feedback?_sort=id&_order=desc"
+    );
+    const data = await response.json();
+    setFeedback(data);
+  };
 
   const addFeedback = (newFeedback) => {
     // Generate an id using uuid4 and add that to newFeedback
@@ -45,6 +42,7 @@ export const FeedbackProvider = ({ children }) => {
       edit: true,
     });
   };
+
   const updateFeedback = (id, updatedItem) => {
     setFeedback(
       feedback.map((item) =>
